6557d74e2e24f4f5544aa427a6166ec1e469024d
Aloquendiar <48574329+Aloquendiar@users.noreply.github.com>
2025-09-30 02:05:19 -0600

    no more song_update

--- dependencies/python/configure_build.py
+++ dependencies/python/configure_build.py
@@ -50,7 +50,6 @@ match sys.platform:
         ninja.rule("copy", "cmd /c copy $in $out $silence", description="COPY $in")
         ninja.rule("bswap", "dependencies\\windows\\swap_art_bytes.exe $in $out", description="BSWAP $in")
         ninja.rule("version", "python dependencies\\python\\gen_version.py $out", description="Writing version info")
-        ninja.rule("song_update_hash", "python dependencies\\python\\gen_song_update_hash.py $out", description="Writing song hash")
         ninja.rule("defines", "python dependencies\\python\\gen_defines.py $out $defines", description="Generating build defines")
         ninja.rule("png_list", "python dependencies\\python\\png_list.py $dir $out", description="PNGLIST $dir")
         ninja.rule("generate_theme_data", "python dependencies\\python\\generate_theme_data.py $dir $out", description="THEMEDATA $dir")
@@ -69,7 +68,6 @@ match sys.platform:
         ninja.rule("copy", "cp $in $out", description="COPY $in")
         ninja.rule("bswap", "python3 dependencies/python/swap_rb_art_bytes.py $in $out", description="BSWAP $in")
         ninja.rule("version", "python3 dependencies/python/gen_version.py $out", description="Writing version info")
-        ninja.rule("song_update_hash", "python3 dependencies/python/gen_song_update_hash.py $out", description="Writing song hash")
         ninja.rule("defines", "python3 dependencies/python/gen_defines.py $out $defines", description="Generating build defines")
         ninja.rule("png_list", "python3 dependencies/python/png_list.py $dir $out", description="PNGLIST $dir")
         ninja.rule("generate_theme_data", "python3 dependencies/python/generate_theme_data.py $dir $out", description="THEMEDATA $dir")
@@ -88,7 +86,6 @@ match sys.platform:
         ninja.rule("copy", "cp --reflink=auto $in $out",description="COPY $in")
         ninja.rule("bswap", "dependencies/linux/swap_art_bytes $in $out", "BSWAP $in")
         ninja.rule("version", "python dependencies/python/gen_version.py $out", description="Writing version info")
-        ninja.rule("song_update_hash", "python dependencies/python/gen_song_update_hash.py $out", description="Writing song hash")
         ninja.rule("defines", "python dependencies/python/gen_defines.py $out $defines", description="Generating build defines")
         ninja.rule("png_list", "python dependencies/python/png_list.py $dir $out", description="PNGLIST $dir")
         ninja.rule("generate_theme_data", "python dependencies/python/generate_theme_data.py $dir $out", description="THEMEDATA $dir")
@@ -271,13 +268,13 @@ ninja.build(str(enc), "dtab_encrypt", str(dtb))
 ark_files.append(str(enc))
 
 # generate song update hash
-dta = Path("obj", args.platform, "raw", "dx", "dx_song_update_hash.dta")
-dtb = Path("obj", args.platform, "raw", "dx", "gen", "dx_song_update_hash.dtb")
-enc = Path("obj", args.platform, "ark", "dx", "gen", "dx_song_update_hash.dtb")
+#dta = Path("obj", args.platform, "raw", "dx", "dx_song_update_hash.dta")
+#dtb = Path("obj", args.platform, "raw", "dx", "gen", "dx_song_update_hash.dtb")
+#enc = Path("obj", args.platform, "ark", "dx", "gen", "dx_song_update_hash.dtb")
 
-ninja.build(str(dta), "song_update_hash", implicit="_always")
-ninja.build(str(dtb), "dtab_serialize", str(dta))
-ninja.build(str(enc), "dtab_encrypt", str(dtb))
+#ninja.build(str(dta), "song_update_hash", implicit="_always")
+#ninja.build(str(dtb), "dtab_serialize", str(dta))
+#ninja.build(str(enc), "dtab_encrypt", str(dtb))
 
 ark_files.append(str(enc))
 
